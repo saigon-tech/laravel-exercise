1fa34c2982c4048c8326def8dd68ac0fdde129bf
ndvkhang.sgt <ndvkhang.sgt@gmail.com>
2023-10-02 10:24:58 +0700

    Issue #2: Comments of LoginController's function edited

--- backend/app/Http/Controllers/LoginController.php
+++ backend/app/Http/Controllers/LoginController.php
@@ -11,33 +11,30 @@ use JetBrains\PhpStorm\NoReturn;
 
 class LoginController extends Controller
 {
-    public function getLogin():view
+    /**
+     * Display login page with authenticated process.
+     * @return View
+     */
+    public function getLogin(): view
     {
-        /**
-         * When user try to reach the login page, this function do a check:
-         * If user logged in ( through check() function) they will be redirected to the welcome page.
-         * If they didn't , they will be able to access the login page.
-         */
         if (Auth::guard('admins')->check()) {
             return view('welcome');
         } else {
             return view('login');
         }
     }
+
+    /**
+     * Validate, check authentication and redirect user after login.
+     * @param  Request  $request
+     * @return RedirectResponse
+     */
     public function postLogin(Request $request): RedirectResponse
     {
-        /**
-         * This step takes the values from the input form and check validation before sending them to authenticating progress.
-         */
         $credentials = $request->validate([
             'username' => 'required|min:6',
             'password' => 'required',
         ]);
-        /**
-         * When the values passed the validating test this step will check the authentication for user:
-         * If the values work, refresh the session and redirect to the dashboard.
-         * If the values did not match, throw an error after redirected them to the login page.
-         */
         if (Auth::guard('admins')->attempt($credentials)) {
             $request->session()->regenerate();
             return redirect()->intended('/');
--- backend/lang/en/auth.php
+++ backend/lang/en/auth.php
@@ -17,5 +17,4 @@ return [
     'password' => 'The provided password is incorrect.',
     'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
     'login_fail' => 'Something wrong wrong',
-
 ];
